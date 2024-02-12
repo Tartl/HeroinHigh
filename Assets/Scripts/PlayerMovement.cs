7d8c3e192b98b25c763c2b1b5d343862ba7c8d49
BlackLover69 <fejtaj.06@spst.eu>
2024-02-12 11:21:57 +0100

    Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+    }
+
+    public void Stop()
+    {
+        GetComponent<PlayerMovement>().runSpeed = 0f;
+    }
+
+    public void Go()
+    {
+        GetComponent<PlayerMovement>().runSpeed = 25f;
+    }
+
+    private void FixedUpdate()
+    {
+        //Moving our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime);
+    }
+}
