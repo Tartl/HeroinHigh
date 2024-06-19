cf1740030dc1145e4b1ed2fbc3655e5c64fd9fa9
Grznar <AsKronak@seznam.cz>
2024-06-19 19:18:30 +0200

    PAUSE MENU

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -2,49 +2,70 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseMenu;
     public static bool isPaused;
-    // Start is called before the first frame update
+
     void Start()
     {
-        pauseMenu.SetActive(false);
+        if (pauseMenu != null)
+        {
+            pauseMenu.SetActive(false);
+        }
+        else
+        {
+            Debug.LogError("Pause menu GameObject is not assigned in the inspector!");
+        }
+        isPaused = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (isPaused)
             {
                 ResumeGame();
-
             }
             else
             {
                 PauseGame();
             }
         }
+
+        if (isPaused && Input.GetKeyDown(KeyCode.M))
+        {
+            GoToMainMenu(); // Pokud je hra pozastavena a hráè stiskne M, vrátí se do hlavního menu
+        }
     }
+
     public void PauseGame()
     {
         isPaused = true;
-        pauseMenu.SetActive(true);
-        Time.timeScale = 0f;
+        if (pauseMenu != null)
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0f; // Zastaví bìh hry
+            Debug.Log("Game paused");
+        }
     }
+
     public void ResumeGame()
     {
         isPaused = false;
-        pauseMenu.SetActive(false);
-        Time.timeScale = 1f;
+        if (pauseMenu != null)
+        {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1f; // Obnoví bìh hry
+            Debug.Log("Game resumed");
+        }
     }
-    public void goToMainMenu()
-    {
-        Time.timeScale = 1f;
-        SceneManager.LoadScene("MejnMenu");
 
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f; // Nastaví èas na normální hodnotu (pokud byl náhodou pozastaven)
+        SceneManager.LoadScene("MejnMenu"); // Naète scénu s hlavním menu
     }
-    
 }
